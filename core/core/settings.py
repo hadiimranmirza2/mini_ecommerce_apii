add1f27134caf317a1d020978d0dde49b80017a6
MirzaHadi2 <mirzahadiimran@gmail.com>
2026-03-19 19:32:10 +0500

    Fix: Configure DATABASES to use DATABASE_URL for Railway

--- core/core/settings.py
+++ core/core/settings.py
@@ -96,9 +96,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
     )
-} 
+}
 
 
 # Password validation
