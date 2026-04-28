24377bbde8dbd9595176f5c8dd3c9ec7e72b3351
MirzaHadi2 <mirzahadiimran@gmail.com>
2026-04-28 19:34:16 +0500

    Update ALLOWED_HOSTS for Northflank deployment

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['miniecommerceapii-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
